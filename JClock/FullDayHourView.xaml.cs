c18296de3945c0ee41c21a55260b7b1b3d7db80e
sashazjukov <sashazjukov@gmail.com>
2020-06-15 17:51:36 +0300

    commit pending changes

--- JClock/FullDayHourView.xaml.cs
+++ JClock/FullDayHourView.xaml.cs
@@ -21,54 +21,103 @@ namespace JClock
     public partial class FullDayHourView : UserControl
     {
         private IList<HourElement> houerElements = new List<HourElement>();
+        private int _hourShift;
+        private bool _inititlized = false;
 
         public void UpdateHourProgress(DateTime dateTime)
         {
-            int hour = dateTime.Hour-6;
-            if (hour > 12)
+            
+            if (!(dateTime.Second == 0 || _inititlized == false)) return;
+            _inititlized = true;
+            int hour = dateTime.Hour- _hourShift;
+            if (hour > 24)
             {
-                hour = 12;
+                hour = 24;
 
             }
+            if (hour < 0)
+            {
+                hour = 24 + hour;
+            }
+            hour = dateTime.Hour;
+
             int i;
-            for (i = 0; i < hour; i++)
+            //for (i = 0; i < hour; i++)
+            //{
+            //    houerElements[i].HourProgressElement.Value = 60;
+            //    houerElements[i].hourText.FontWeight = FontWeights.Light;
+            //}
+            //for (i = hour; i < 24; i++)
+            //{
+            //    houerElements[i].HourProgressElement.Value = 0;
+            //    houerElements[i].hourText.FontWeight = FontWeights.Light;
+            //}
+            //// curent hour
+            //if (hour < 24)
+            //{
+            //    houerElements[hour].HourProgressElement.Value = dateTime.Minute;
+            //    houerElements[hour].hourText.FontWeight = FontWeights.Bold;
+            //}
+            //else
+            //{
+            //    houerElements[hour-1].HourProgressElement.Value = 60;
+            //    houerElements[hour-1].hourText.FontWeight = FontWeights.Light;
+
+            for (i = 0; i < 24; i++)
             {
-                houerElements[i].HourProgressElement.Value = 60;
-                houerElements[i].hourText.FontWeight = FontWeights.Light;
-            }
-            for (i = hour; i < 12; i++)
-            {
-                houerElements[i].HourProgressElement.Value = 0;
-                houerElements[i].hourText.FontWeight = FontWeights.Light;
-            }
-            // curent hour
-            if (hour < 12)
-            {
-                houerElements[hour].HourProgressElement.Value = dateTime.Minute;
-                houerElements[hour].hourText.FontWeight = FontWeights.Bold;
-            }
-            else
-            {
-                houerElements[hour-1].HourProgressElement.Value = 60;
-                houerElements[hour-1].hourText.FontWeight = FontWeights.Light;
+                if (houerElements[i].hoourValue < hour)
+                {
+                    houerElements[i].HourProgressElement.Value = 60;
+                    houerElements[i].hourText.FontWeight = FontWeights.Light;
+                }
+                if (houerElements[i].hoourValue > hour)
+                {
+                    houerElements[i].HourProgressElement.Value = 0;
+                    houerElements[i].hourText.FontWeight = FontWeights.Light;
+                }
+                if (houerElements[i].hoourValue == hour)
+                {
+                    houerElements[i].HourProgressElement.Value = dateTime.Minute;
+                    houerElements[i].hourText.FontWeight = FontWeights.Bold;  
+                }
+
             }
         }
 
         public FullDayHourView()
         {
-            InitializeComponent();            
-            int p = 0;
-            for (int i = 0; i < 12; i++)
+            InitializeComponent();
+            _hourShift = 8;
+            int col = 0;
+            int row = 0;
+            int counter = 0;
+            int hourElementHoourValue = _hourShift;
+            for (int i = 0; i < 24; i++)
             {
                 HourElement hourElement = new HourElement();
                 houerElements.Add(hourElement);
                 ParentGrid.Children.Add(hourElement);
 
                 //rectangle.hourText.Text = (i+6).ToString();
-                hourElement.hoourValue = (i+6);
-                Grid.SetColumn(hourElement, p);
-                if ((i + 1) % 3 == 0) p++;
-                p++;                
+
+                
+                hourElement.hoourValue = hourElementHoourValue;
+                Grid.SetColumn(hourElement, col);
+                Grid.SetRow(hourElement, row);
+                if ((counter + 1) % 3 == 0) col++;
+                counter++;
+                col++;
+                if (i == 11) {
+                    row += 2;
+                    col = 0;
+                    counter = 0;
+                }
+                hourElementHoourValue++;
+                if (hourElementHoourValue == 24)
+                {
+                    hourElementHoourValue = 0;                    
+                }
+
             } 
             UpdateHourProgress(DateTime.Now);
         }
--- JClock/MainWindow.xaml.cs
+++ JClock/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -29,14 +31,16 @@ namespace JClock
         {
             InitializeComponent();
             DataContext = this;
-            _timer = new Timer(Callback, null,0,1000);
-            this.MouseDown +=Window_MouseDown;
+            _timer = new Timer(Callback, null, 0, 1000);
+            this.MouseDown += Window_MouseDown;
             TimeNow = DateTime.Now;
-            this.MouseMove+=OnMouseMove;
-            this.MouseLeave+=OnMouseLeave;
+            this.MouseMove += OnMouseMove;
+            this.MouseLeave += OnMouseLeave;
 
             BClose.Visibility = Visibility.Collapsed;
             BTitle.Visibility = Visibility.Collapsed;
+            BTitle.Content = "JClock 1.0";
+            this.Loaded += Window_Loaded;
         }
 
         private void OnMouseLeave(object sender, MouseEventArgs mouseEventArgs)
@@ -70,7 +74,7 @@ namespace JClock
                 TimeNow = DateTime.Now;
                 FullDayHourViewElement.UpdateHourProgress(TimeNow);
             }));
-            
+
         }
 
         private void UIElement_OnMouseMove(object sender, MouseEventArgs e)
@@ -93,5 +97,79 @@ namespace JClock
             BClose.Background = Brushes.Red;
             e.Handled = true;
         }
+
+
+        internal enum AccentState
+        {
+            ACCENT_DISABLED = 1,
+            ACCENT_ENABLE_GRADIENT = 0,
+            ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+            ACCENT_ENABLE_BLURBEHIND = 3,
+            ACCENT_INVALID_STATE = 4
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct AccentPolicy
+        {
+            public AccentState AccentState;
+            public int AccentFlags;
+            public int GradientColor;
+            public int AnimationId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct WindowCompositionAttributeData
+        {
+            public WindowCompositionAttribute Attribute;
+            public IntPtr Data;
+            public int SizeOfData;
+        }
+
+        internal enum WindowCompositionAttribute
+        {
+            // ...
+            WCA_ACCENT_POLICY = 19
+
+            // ...
+        }
+
+        /// <summary>
+        /// Interaction logic for MainWindow.xaml
+        /// </summary>
+
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            EnableBlur();
+        }
+
+        internal void EnableBlur()
+        {
+            var windowHelper = new WindowInteropHelper(this);
+
+            var accent = new AccentPolicy();
+            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+
+            var accentStructSize = Marshal.SizeOf(accent);
+
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData();
+            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+            data.SizeOfData = accentStructSize;
+            data.Data = accentPtr;
+
+            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+
+
     }
+
+
 }
